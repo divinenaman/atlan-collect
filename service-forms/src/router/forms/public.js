5512f2f0826a125a0a619ad5382a2d1ed6e731b3
Naman Agarwal <namanddev7@gmail.com>
2022-09-15 19:02:09 +0530

    rm

--- service-forms/src/router/forms/public.js
+++
@@ -1,8 +0,0 @@
-const formsController = require("../../controllers/forms/public");
-
-const createMemberRoutes = (r) => {
-  r.get("/:formId/view", formsController.view);
-  r.post("/:formId/submit", formsController.submit);
-};
-
-module.exports = createMemberRoutes;
